3baf54c3d01446cea40b21fdfe169be56738bc1d
Marek Rawdanowicz <marek.rawdanowicz@outlook.com>
2020-11-20 22:21:06 +0100

    update page not found, css changes

--- src/app/shared/page-not-found/page-not-found.component.ts
+++ src/app/shared/page-not-found/page-not-found.component.ts
@@ -2,15 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-page-not-found',
-  template: `<h2>Page not found!</h2>`,
-  styles: [
-    `
-      h2 {
-        position: absolute;
-        left: 50%;
-        transform: translateX(-50%);
-      }
-    `,
-  ],
+  templateUrl: 'page-not-found.component.html',
+  styleUrls: ['page-not-found.component.css'],
 })
 export class PageNotFoundComponent {}
